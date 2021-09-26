927127993242dbaaf3a24751cad1e52ab1efbbeb
marleigh <volcana@gmail.com>
2021-09-26 12:51:31 -0700

    converting to state based. fixing inconsistencies

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 
-import TodoItem from "./components/TodoItem";
+import ToDoItem from "./components/TodoItem";
 import Header from "./components/Header";
 import toDoData from "./toDoData";
+import React from "react";
 
-function App() {
-  const toDoComponents = toDoData.map(data => <TodoItem key={data.id} data={data}/>)
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: toDoData
+    }
+  }
+  render() {
+    const toDoComponents = this.state.todos.map(data => <ToDoItem key={data.id} data={data}/>)
 
-  return (
-    <div className="container">
-      <Header />
-      <div className="list-container">
-        {toDoComponents}
+    return (
+      <div className="container">
+        <Header />
+        <div className="list-container">
+          {toDoComponents}
+        </div>
       </div>
-    </div>
+  
+    );
 
-  );
+  }
+  
 }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,24 +1,25 @@
 import React from "react"
 
-// function TodoList(props) {
-//     return (
-//         <div className="app"> 
-//           <input type="checkbox" name="item" />
-//           <p className="checkbox-item">{props.data.text}</p>
-//         </div>
-//     )
-
-// }
-
-class ToDoList extends React.Component {
-  render() {
-      return (
+function ToDoList(props) {
+    return (
         <div className="app"> 
-        <input type="checkbox" name="item" />
-        <p className="checkbox-item">{this.props.data.text}</p>
-      </div>
-      )
-  }
+          <input type="checkbox" name="item" />
+          <p className="checkbox-item">{props.data.text}</p>
+        </div>
+    )
+
 }
+
+// class ToDoList extends React.Component {
+
+//   render() {
+//       return (
+//         <div className="app"> 
+//         <input type="checkbox" name="item" />
+//         <p className="checkbox-item">{this.state.data}</p>
+//       </div>
+//       )
+//   }
+// }
 
 export default ToDoList;
